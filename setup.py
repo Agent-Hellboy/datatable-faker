77863c62661921d0bec9e5c8dd524d06d137ddc0
Prince Roshan <princekrroshan01@gmail.com>
2023-04-25 19:14:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 setup(
     name="datatable-faker",
     author="Prince Roshan",
-    version='0.1.0',
+    version='0.1.2',
     author_email="princekrroshan01@gmail.com",
     url="https://github.com/Agent-Hellboy/datatable-faker",
     description=("Library to generate fake datatable for unittest "),
